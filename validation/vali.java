93c2910b0315b4021401e2c30de8e85c5189636a
Soni R <93289594+Soni-20@users.noreply.github.com>
2022-08-31 11:44:48 +0530

    Update vali.java

--- validation/vali.java
+++ validation/vali.java
@@ -17,12 +17,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String username="Soni-20";
-                int password=123;
+                String password="123";
                 EditText t1 =(EditText)  findViewById(R.id.username);
                 EditText t2 =(EditText) findViewById(R.id.password);
                 String s1 =t1.getText().toString();
                 String s2= t2.getText().toString();
-                if(s1.equals("Soni-20") && s2.equals(123))
+                if(s1.equals("Soni-20") && s2.equals("123"))
                 {
                     Toast.makeText(MainActivity.this, "successs", Toast.LENGTH_SHORT).show();
                 }
